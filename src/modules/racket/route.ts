d651dd75dc6c816c31c0ca7ca1433a417735f25f
rudihedev <rudihedev@gmail.com>
2026-01-02 22:52:48 +0700

    feat: .env.example

--- src/modules/racket/route.ts
+++ src/modules/racket/route.ts
@@ -18,3 +18,32 @@ racketRoute.get("/:slug", (c) => {
 
   return c.json(foundRacket);
 });
+
+racketRoute.post("/", async (c) => {
+  try {
+    const body = await c.req.json();
+
+    // Check for validation
+    if (!body.slug || !body.name) {
+      return c.json({ error: "slug dan name wajib diisi" }, 400);
+    }
+
+    // Duplication checking
+    const exists = dataRackets.find((racket) => racket.slug === body.slug);
+    if (exists) {
+      return c.json({ error: "Racket dengan slug ini sudah ada" }, 409);
+    }
+
+    // Add new racket
+    const newRacket = {
+      id: dataRackets.length + 1, // atau generate ID yang lebih robust
+      ...body,
+    };
+
+    dataRackets.push(newRacket);
+
+    return c.json(newRacket, 201);
+  } catch (error) {
+    return c.json({ error: "Invalid JSON" }, 400);
+  }
+});
