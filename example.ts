bdae11a3bf2b590a92a6ab5907845e0b5cf88424
rudihedev <rudihedev@gmail.com>
2026-01-12 14:20:37 +0700

    feat: setup config for typescript

--- example.ts
+++ example.ts
@@ -1,14 +1,26 @@
 import * as pg from "pg";
+
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
 });
 
 await client.connect();
 
-const result = await client.query("SELECT * FROM racketo");
+type Racket = {
+  id: number;
+  brand: string;
+  name: number;
+  slug: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-const rackets = result.rows;
-
-console.log({ rackets });
-
-await client.end();
+try {
+  const result = await client.query("SELECT * FROM racketo");
+  const rackets: Racket[] = result.rows;
+  console.log({ rackets });
+} catch (error) {
+  console.error("Failed to connect to the database", error);
+} finally {
+  await client.end();
+}
