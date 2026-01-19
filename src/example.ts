e304a0a7f043e0d5c0bbb6a809360b0a49ce1d88
rudihedev <rudihedev@gmail.com>
2026-01-19 13:53:56 +0700

    feat: scripts package.json

--- src/example.ts
+++ src/example.ts
@@ -1,25 +1,21 @@
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  const newRacket = await prisma.racket.create({
-    data: {
-      brand: "Flypower",
-      name: "Tornado 800",
-      slug: "tornado-800",
-      weight: "3U",
-    },
-  });
-  console.log("Created racket:", newRacket);
-}
+  // const newRacket = await prisma.racket.create({
+  //   data: {
+  //     brand: "HiQua",
+  //     name: "Smasher 900",
+  //     slug: "smasher-900",
+  //     weight: "4U",
+  //   },
+  // });
+  // console.log("Created racket:", newRacket);
 
-// Fetch all users with their posts
-//   const allUsers = await prisma.user.findMany({
-//     include: {
-//       posts: true,
-//     },
-//   });
-//   console.log("All users:", JSON.stringify(allUsers, null, 2));
-// }
+  const allRackets = await prisma.racket.findMany();
+
+  // Fetch all users with their posts
+  console.log(allRackets);
+}
 
 main()
   .then(async () => {
