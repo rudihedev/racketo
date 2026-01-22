cef29b28e296ed44693e4b89db38b4f4467b205a
rudihedev <rudihedev@gmail.com>
2026-01-22 21:23:06 +0700

    Revise route.ts

--- src/modules/racket/route.ts
+++ src/modules/racket/route.ts
@@ -18,7 +18,7 @@ racketRoute.get("/:slug", async (c) => {
   const racket = await prisma.racket.findUnique({
     where: { slug },
   });
-  console.log(slug);
+  console.log("slug:", slug);
   return c.json({});
 });
 
